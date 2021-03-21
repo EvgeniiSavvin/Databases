f8481e8d4244d7f92ef34c58c01d9074076e93b9
1337BLENDER <savvin_evgenii@mail.ru>
2021-03-21 21:26:13 +0300

    Add scripts for postgres initialization

---
+++ postgres/initPostgres.bat
@@ -0,0 +1,6 @@
+set container=PostgresForDb
+docker stop %container%
+docker container rm %container%
+docker run -d --name %container% -p 5432:5432 -e POSTGRES_PASSWORD=itmo -e POSTGRES_USER=itmo -e POSTGRES_DB=itmo postgres:11.5-alpine
+timeout 3 > NUL
+type dbInit.sql | docker exec -i %container% psql -U itmo -d itmo
